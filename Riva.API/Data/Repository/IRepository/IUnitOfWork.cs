a5300d2806895cb436d052307c53971b850bc7a4
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2026-01-30 15:10:25 +0100

    Add public void Save() => _db.SaveChanges();

---
+++ Riva.API/Data/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace Riva.API.Data.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        IVillaRepository villaRepository { get; }
+
+        void Save();
+        Task Saveasync();
+    }
+}
---
+++ Riva.API/Data/Repository/Repositories/VillaRepository.cs
@@ -0,0 +1,13 @@
+﻿using Riva.API.Data.Context;
+using Riva.API.Data.Repository.IRepository;
+using Riva.API.Models;
+
+namespace Riva.API.Data.Repository.Repositories
+{
+    public class VillaRepository : Repository<Villa>, IVillaRepository
+    {
+        public VillaRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+    }
+}
