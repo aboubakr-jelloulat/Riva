8734e7e3c52927e9ee0f94cb649e988114ea8d02
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2026-01-30 15:15:48 +0100

    Reapply "Add public void Save() => _db.SaveChanges();"
    This reverts commit d226fe5b962c9345e01e0384cfaa6bb99583a9cf.

---
+++ Riva.API/Data/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,12 @@
+﻿using Riva.API.Models;
+
+namespace Riva.API.Data.Repository.IRepository
+{
+    public interface IVillaRepository : IRepository<Villa>
+    {
+        /*
+         * you can add custom methods only for Villa like:
+                GetVillaByxxx(string name)
+        */
+    }
+}
